db2502514646ad0a0afb94bf6912b81c2852ab33
Thiago Rizzo <thiagorizzo@gmail.com>
2022-10-26 14:38:11 -0300

    added b-tree remove operation

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -35,6 +35,76 @@ bool inserir(No **raiz, int valor) {
 	}
 }
 
+No* buscarNoMaiorDireita(No **raiz){
+    if ((*raiz)->direita != NULL) 
+       return buscarNoMaiorDireita(&(*raiz)->direita);
+    else {
+       No *aux = *raiz;
+
+       if((*raiz)->esquerda != NULL)
+          *raiz = (*raiz)->esquerda;
+       else
+          *raiz = NULL;
+
+       return aux;
+   }
+}
+
+No* buscarNoMenorEsquerda(No **raiz){
+    if ((*raiz)->esquerda != NULL) 
+       return buscarNoMenorEsquerda(&(*raiz)->esquerda);
+    else{
+       No *aux = *raiz; 
+       
+	   if ((*raiz)->direita != NULL)
+          *raiz = (*raiz)->direita;
+       else
+          *raiz = NULL;
+
+	   return aux;
+   }
+}
+
+bool remover(No **raiz, int valor){
+    if (*raiz == NULL) {
+		return false;
+    }
+    
+    if (valor < (*raiz)->valor)
+    	remover(&(*raiz)->esquerda, valor);
+    else 
+       if (valor > (*raiz)->valor) {
+          remover(&(*raiz)->direita, valor);
+       } else {
+          No *aux = *raiz;
+          
+		  if (((*raiz)->esquerda == NULL) && ((*raiz)->direita == NULL)) {
+            (*raiz) = NULL; 
+          } else {
+             if ((*raiz)->esquerda == NULL) {
+                (*raiz) = (*raiz)->direita;
+                aux->direita = NULL;
+                aux = NULL;
+            } else {
+                if ((*raiz)->direita == NULL) {
+                    (*raiz) = (*raiz)->esquerda;
+                    aux->esquerda = NULL;
+                    aux = NULL;
+            	} else {
+                   aux = buscarNoMaiorDireita(&(*raiz)->esquerda);
+                   aux->esquerda = (*raiz)->esquerda;
+                   aux->direita = (*raiz)->direita;
+                   (*raiz)->esquerda = (*raiz)->direita = NULL;
+				   *raiz = aux;
+				   aux = NULL;   
+                }
+            }
+        }
+    }
+    
+    return true;
+}
+
 void preordem(No *raiz) {
 	if (!estaVazia(&raiz)) {
 		printf("%d\n", raiz->valor);
@@ -74,7 +144,8 @@ int main() {
         printf("2) Préordem.\n");
         printf("3) Inordem.\n");
         printf("4) Pósordem.\n");
-        printf("5) Sair.\n");
+        printf("5) Remover.\n");
+        printf("6) Sair.\n");
         scanf("%d", &opcao);
 
 		switch(opcao) {
@@ -82,33 +153,45 @@ int main() {
                 printf("digite um número:");
                 scanf("%d", &numero);
                 
-                bool resultado = inserir(&arvore, numero);
-                if (resultado == false) 
-                	printf("Não foi inserir. Valor já existente.");
+                bool resultadoInserir = inserir(&arvore, numero);
+                if (resultadoInserir == false) 
+                	printf("Não foi possível inserir. Valor já existe.");
                 else
                 	printf("Valor inserido com sucesso.");
                 	
                 break;
                 
             case 2:
+                printf("digite um número:");
+                scanf("%d", &numero);
+                
+                bool resultadoRemover = remover(&arvore, numero);
+                if (resultadoRemover == false) 
+                	printf("Não foi possível remover. Valor inexistente.");
+                else
+                	printf("Valor removido com sucesso.");
+                	
+                break;
+                
+            case 3:
     			printf("==============\n");            	
             	printf("Préordem:\n");
                 preordem(arvore);
                 break;   
 
-            case 3:
+            case 4:
     			printf("==============\n");            	
             	printf("Inordem:\n");
                 inordem(arvore);
                 break;   
 
-            case 4:
+            case 5:
     			printf("==============\n");            	
             	printf("Pósordem:\n");
                 posordem(arvore);
                 break;   
         }
-    } while(opcao != 5);
+    } while(opcao != 6);
     
     return 0;
 }
